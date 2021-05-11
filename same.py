89b12aef36af86dc13e5a80b6dbda51baf33186f
hssneha03 <hssneha03@gmail.com>
2021-05-12 01:23:22 +0530

    refer same.py for pre-processing

---
+++ same.py
@@ -0,0 +1,75 @@
+import json
+import math
+import os
+import cv2
+from PIL import Image
+import numpy as np
+from keras import layers
+from keras.applications import ResNet50,MobileNet, DenseNet201, InceptionV3, NASNetLarge, InceptionResNetV2, NASNetMobile
+from keras.callbacks import Callback, ModelCheckpoint, ReduceLROnPlateau, TensorBoard
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import cohen_kappa_score, accuracy_score
+import scipy
+from tqdm import tqdm
+import tensorflow as tf
+from keras import backend as K
+import gc
+from functools import partial
+from sklearn import metrics
+from collections import Counter
+import json
+import itertools
+
+#loading data and preprocessing
+def Dataset_loader(DIR, RESIZE, sigmaX=10):
+    IMG = []
+    read = lambda imname: np.asarray(Image.open(imname).convert("RGB"))
+    for IMAGE_NAME in tqdm(os.listdir(DIR)):
+        PATH = os.path.join(DIR,IMAGE_NAME)
+        _, ftype = os.path.splitext(PATH)
+        if ftype == ".jpg":
+            img = read(PATH)
+           
+            img = cv2.resize(img, (RESIZE,RESIZE))
+           
+            IMG.append(np.array(img))
+    return IMG
+
+eczema_train = np.array(Dataset_loader("/home/sneha/Documents/medical-image-analysis/train/Eczema Photos", 224))
+melanoma_train = np.array(Dataset_loader("/home/sneha/Documents/medical-image-analysis/train/Melanoma Skin Cancer Nevi and Moles",224))
+psoriasis_train = np.array(Dataset_loader("/home/sneha/Documents/medical-image-analysis/train/Psoriasis pictures Lichen Planus and related diseases",224))
+eczema_test = np.array(Dataset_loader("/home/sneha/Documents/medical-image-analysis/test/Eczema Photos",224))
+melonoma_test = np.array(Dataset_loader("/home/sneha/Documents/medical-image-analysis/test/Melanoma Skin Cancer Nevi and Moles",224))
+psoriasis_test = np.array(Dataset_loader("/home/sneha/Documents/medical-image-analysis/test/Psoriasis pictures Lichen Planus and related diseases", 224))
+
+#labelling 
+eczema_train_label = np.zeros(len(eczema_train))
+melonoma_train_label = np.zeros(len(melanoma_train))
+psoriasis_train_label = np.zeros(len(psoriasis_train))
+eczema_test_label = np.zeros(len(eczema_test))
+melonoma_test_label = np.zeros(len(melonoma_test))
+psoriasis_test_label = np.zeros(len(psoriasis_test))
+
+X_train = np.concatenate((eczema_train, melanoma_train, psoriasis_train), axis=0)
+Y_train = np.concatenate((eczema_train_label, melonoma_train_label, psoriasis_train_label), axis=0)
+X_test = np.concatenate((eczema_test, melonoma_test, psoriasis_test), axis=0)
+Y_test = np.concatenate((eczema_test_label, melonoma_test_label, psoriasis_test_label), axis = 0)
+
+s = np.arange(X_train.shape[0])
+np.random.shuffle(s)
+X_train = X_train[s]
+Y_train = Y_train[s]
+
+s = np.arange(X_test.shape[0])
+np.random.shuffle(s)
+X_test = X_test[s]
+Y_test = Y_test[s]
+
+Y_train = to_categorical(Y_train, num_classes= 3)
+Y_test = to_categorical(Y_test, num_classes= 3)
